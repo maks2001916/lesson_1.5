a53cc881ea4d0e148b160484d94e9f5495ff6b5e
maks <mamonov.maksim.20003@gmail.com>
2022-08-12 19:48:01 +0300

    тертье задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,14 @@ public class Main {
         double [] numbersTwo = {1.57, 7.654, 9.986};
         int [] numbersThree = {5, 8, 13, 21, 49, 58};
         //второе задание
-        System.out.print(numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
-        System.out.println();
-        System.out.print(numbersTwo[0] + ", " + numbersTwo[1] + ", " + numbersTwo[2]);
-        System.out.println();
+        System.out.println(numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
+        System.out.println(numbersTwo[0] + ", " + numbersTwo[1] + ", " + numbersTwo[2]);
         System.out.println(numbersThree[0] + ", " + numbersThree[1] + ", " + numbersThree[2] + ", " + numbersThree[3] + ", " + numbersThree[4] + ", " + numbersThree[5]);
+        //третье задание
+        System.out.println(numbers[2] + ", " + numbers[1] + ", " + numbers[0]);
+        System.out.println(numbersTwo[2] + ", " + numbersTwo[1] + ", " + numbersTwo[0]);
+        System.out.println(numbersThree[5] + ", " + numbersThree[4] + ", " + numbersThree[3] + ", " + numbersThree[2] + ", " + numbersThree[1] + ", " + numbersThree[0]);
         }
-    }
+        }
+
 
