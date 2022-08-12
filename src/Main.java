3b9cd8cd4e8916b0fe60bb7c93f48dd5ec9dce0c
maks <mamonov.maksim.20003@gmail.com>
2022-08-12 18:32:26 +0300

    первое и второе задания выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //первое задание
         System.out.println("Hello world!");
+        int [] numbers = new int [3];
+        numbers [0] = 1;
+        numbers [1] = 2;
+        numbers [2] = 3;
+        double [] numbersTwo = {1.57, 7.654, 9.986};
+        int [] numbersThree = {5, 8, 13, 21, 49, 58};
+        //второе задание
+        System.out.print(numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
+        System.out.println();
+        System.out.print(numbersTwo[0] + ", " + numbersTwo[1] + ", " + numbersTwo[2]);
+        System.out.println();
+        System.out.println(numbersThree[0] + ", " + numbersThree[1] + ", " + numbersThree[2] + ", " + numbersThree[3] + ", " + numbersThree[4] + ", " + numbersThree[5]);
+        }
     }
-}
+
